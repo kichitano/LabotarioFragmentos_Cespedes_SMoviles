a34470fae2bdbe081db11b8544e2e282d4a6fad9
kichitano <30935429+kichitano@users.noreply.github.com>
2019-08-26 20:49:50 -0500

    Avance LabFragmentos

--- app/src/main/java/com/example/labfragmentos/MainActivity.java
+++ app/src/main/java/com/example/labfragmentos/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParrafoFragment parrafoFragment = new ParrafoFragment();
+/*
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, parrafoFragment)
+                .commit();
+*/
     }
 }
